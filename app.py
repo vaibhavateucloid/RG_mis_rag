2ceb0c56252c474783d7b3085e685fc63889254b
Vaibhav Shelar <vaibhav.shelar@eucloid.com>
2025-07-04 14:02:23 +0530

    made the retrival and generation layer

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 # This is the main entrypoint for the user and will be a streamlit app which will internally call the rag_main.py
+
+from rag_main import RAGSystem
+
+rag = RAGSystem()
+result = rag.query("Which is the fastest growing demand booster?")
